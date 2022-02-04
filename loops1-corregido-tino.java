d2c529838e43d3a39a1e9fd107aba5cc2373f9bd
concer01 <95487501+concer01@users.noreply.github.com>
2022-02-04 20:00:21 +0100

    Create loops1-corregido-tino.java

---
+++ loops1-corregido-tino.java
@@ -0,0 +1,14 @@
+bucles de clase  pública1 {
+    public  static  void  main ( String [] args ) lanza  IOException {
+        BufferedReader bufferedReader =  new  BufferedReader ( new  InputStreamReader ( System . in));
+
+        int  N  =  Entero . parseInt( bufferedReader . readLine() . trim());
+        ent yo;
+        para (i = 1 ;i <= 10 ;i)
+        {
+        	sistema _ fuera _ println( N  *  " x "  + i +  " = "  + ( N * i));
+        }
+        
+        lector almacenado en búfer . cerca();
+    }
+}
